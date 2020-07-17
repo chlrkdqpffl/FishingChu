a6dca435885a22b06b13d6372db8dc7683016ffd
chlrkdqpffl <chlrkdqpffl@gmail.com>
2020-07-17 17:04:50 +0900

    - 최초 커밋

---
+++ FishingChu/Assets/Scripts/DiceManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceManager : MonoBehaviour
+{
+	private int[] m_arrDice;
+    void Start()
+    {
+		m_arrDice = new int[5];
+		m_arrDice[0] = 1;
+		m_arrDice[1] = 3;
+		m_arrDice[2] = 1;
+		m_arrDice[3] = 2;
+		m_arrDice[4] = 5;
+
+		ScoreManager.Instance.CalcDiceValue(m_arrDice);
+
+	}
+
+	void Update()
+    {
+		
+	}
+}
---
+++ FishingChu/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,168 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+	// rank of hands
+	public enum RankOfHands
+	{
+		eNone,
+		eChoice,
+		e4ofaKind,
+		eFullHouse,
+		eSmallStraight,
+		eLargeStraight,
+		eFishingchu,
+	}
+
+	public enum Cate
+	public Text[] m_textPlayer1;
+	public Text[] m_textPlayer2;
+
+	private static ScoreManager _instance;
+	public static ScoreManager Instance
+	{
+		get	{
+			if (_instance == null)
+			{
+				_instance = new ScoreManager();
+			}
+			return _instance;
+		}
+	}
+
+//	private Dictionary<int, int> m_dic
+	
+	// 각 족보별 계산식 적용
+	//delegate 
+
+
+
+
+	public void CalcDiceValue(int[] arrDice)
+	{
+
+
+	}
+
+	int CalcTotalSum(int[] arrDice)
+	{
+		int result = 0;
+		foreach (var value in arrDice)
+			result += value;
+
+		return result;
+	}
+
+	bool Check4ofaKind(int[] arrDice)
+	{
+		var dicTemp = new Dictionary<int, int>();
+
+		foreach (var value in arrDice)
+		{
+			if (dicTemp.ContainsKey(arrDice[0]))
+				dicTemp[0]++;
+			else
+				dicTemp.Add(arrDice[0], 1);
+		}
+
+		if (2 < dicTemp.Count)
+			return false;
+
+		foreach(var value in dicTemp)
+		{
+			if (4 <= value.Value)
+				return true;
+		}
+
+		return false;
+	}
+
+	bool CheckFullHouse(int[] arrDice)
+	{
+		var dicTemp = new Dictionary<int, int>();
+
+		foreach (var value in arrDice)
+		{
+			if (dicTemp.ContainsKey(arrDice[0]))
+				dicTemp[0]++;
+			else
+				dicTemp.Add(arrDice[0], 1);
+		}
+
+		if (2 < dicTemp.Count)
+			return false;
+
+		if (dicTemp.Count == 1)
+		{
+			return true;
+		}
+		else
+		{
+			foreach (var value in dicTemp)
+			{
+				if (value.Value == 2 || value.Value == 3)
+					return true;
+			}
+		}
+		
+		return false;
+	}
+
+	int CheckSmallStaright(int[] arrDice)
+	{
+		Array.Sort(arrDice);
+
+		int consecutiveNumber = 0;
+		for (int i = 0; i < arrDice.Length - 1; ++i) {
+			if (arrDice[i] == arrDice[i + 1] + 1)
+				consecutiveNumber++;
+		}
+
+		if (4 <= consecutiveNumber)
+			return 15;
+		else
+			return 0;
+	}
+
+	int CheckLargeStaright(int[] arrDice)
+	{
+		Array.Sort(arrDice);
+
+		int consecutiveNumber = 0;
+		for (int i = 0; i < arrDice.Length - 1; ++i)
+		{
+			if (arrDice[i] == arrDice[i + 1] + 1)
+				consecutiveNumber++;
+		}
+
+		if (5 <= consecutiveNumber)
+			return 30;
+		else
+			return 0;
+	}
+
+	int CheckFishingChu(int[] arrDice)
+	{
+		int selectValue = arrDice[0];
+		foreach(var value in arrDice)
+		{
+			if (selectValue == value)
+				continue;
+			else
+				return 0;
+		}
+
+		return 50;
+	}
+
+	void UpdateScore()
+	{
+		// Text 에 글자 업데이트
+
+
+	}
+}
